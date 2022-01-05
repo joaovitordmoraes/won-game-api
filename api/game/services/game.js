8cb6cc317720ef52bccfc87b44023732b59abafe
Joao Moraes <joaovitordmoraes@gmail.com>
2022-01-04 22:19:37 -0300

    Start populate games with gog api

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -5,4 +5,49 @@
  * to customize this service
  */
 
-module.exports = {};
+const axios = require("axios");
+const slugify = require("slugify");
+
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const description = dom.window.document.querySelector('.description');
+
+  return {
+    rating: 'BR0',
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML
+  }
+}
+
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name });
+
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName);
+
+  if (!item) {
+    return await strapi.services[entityName].create({ name, slug: slugify(name, { lower: true }) });
+  }
+}
+
+module.exports = {
+  populate: async (params) => {
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+
+    const { data: { products } } = await axios.get(gogApiUrl);
+
+    console.log(products[0]);
+
+    await create(products[2].publisher, "publisher");
+    await create(products[2].developer, "developer");
+
+    // console.log(await getGameInfo(products[0].slug));
+  }
+};
