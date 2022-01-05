998d948a38406e27df4e47c269fb2208f1e1f78f
Joao Moraes <joaovitordmoraes@gmail.com>
2022-01-05 17:58:35 -0300

    Create data scraper to populate

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,16 +1,22 @@
-'use strict';
+"use strict";
 
 /**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
 module.exports = {
   populate: async (ctx) => {
-    console.log("Starting to populate...")
+    console.log("Starting to populate...");
 
-    await strapi.services.game.populate()
+    const options = {
+      sort: "popularity",
+      page: "1",
+      ...ctx.query,
+    };
 
-    ctx.send("Finished populating!")
-  }
+    await strapi.services.game.populate(options);
+
+    ctx.send("Finished populating!");
+  },
 };
